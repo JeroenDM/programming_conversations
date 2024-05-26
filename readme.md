4cdf9af934dacaeccf04c4cc1ff87b0f2a5122e4
jeroendm <jeroendemaeyer@live.be>
2024-05-26 22:50:31 +0200

    try out a version with references

---
+++ readme.md
@@ -0,0 +1,7 @@
+
+
+Rust makes it more difficult to the "the wrong thing",
+if I try to follow the course example and create a hash map with iterator pairs to values in a set.
+
+I can only get references that I cannot easily put into a hash map,
+or wrap everything explicitly in ref counter objects.
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 #![feature(iter_map_windows)]
-use std::{collections::HashMap, fs};
+#![feature(hash_set_entry)]
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 use itertools::Itertools;
 
-#[derive(PartialEq, Hash)]
+#[derive(PartialEq, Eq, Hash)]
 struct BiGram {
     w1: String,
     w2: String,
 }
 
-impl Eq for BiGram {}
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct MyPair<'a> {
+    s1: &'a String,
+    s2: &'a String,
+}
 
-fn main() {
-    let content = fs::read_to_string("data/pandp.txt").expect("Failed to read book.");
+fn bigram_v1(filename: &str) {
+    let content = fs::read_to_string(filename).expect("Failed to read book.");
     println!("Read {} characters.", content.len());
 
     let mut bigrams = HashMap::new();
-    // let _iter: Vec<_> = content
-    //     .split_ascii_whitespace()
-    //     // .take(10000)
-    //     .map_windows(|[x, y]| {
-    //         let bg = BiGram {
-    //             w1: x.to_string(),
-    //             w2: y.to_string(),
-    //         };
-    //         bigrams.entry(bg).and_modify(|x| *x += 1).or_insert(1);
-    //     })
-    //     .collect();
 
     for (x, y) in content.split_ascii_whitespace().into_iter().tuple_windows() {
         bigrams
@@ -45,4 +42,61 @@ fn main() {
     for (k, v) in sorted.iter().take(10) {
         println!("{: <18} {}", format!("{}-{} :", k.w1, k.w2), v);
     }
+}
+
+fn bigram_with_extra_set(filename: &str) {
+    let content = fs::read_to_string(filename).expect("Failed to read book.");
+    println!("Read {} characters.", content.len());
+
+    let mut words = HashSet::new();
+    // Helper scope to prove a point later on.
+    {
+        let mut bigrams = HashMap::new();
+
+        // Split this up into two loops,
+        // once we put x_ref into the map,
+        // we cannot borrow words mutable anymore because
+        // the reference is around as long as 'bigrams' is around.
+        for (x, y) in content.split_ascii_whitespace().into_iter().tuple_windows()
+        // .take(5)
+        {
+            words.insert(x.to_string());
+            words.insert(y.to_string());
+        }
+
+        for (x, y) in content.split_ascii_whitespace().into_iter().tuple_windows()
+        // .take(5)
+        {
+            let x_ref = words.get(x).unwrap();
+            let y_ref = words.get(y).unwrap();
+
+            bigrams
+                .entry(MyPair {
+                    s1: x_ref,
+                    s2: y_ref,
+                })
+                .and_modify(|x| *x += 1)
+                .or_insert(1);
+        }
+        // This does not work anymore
+        // words.insert("hello".to_string());
+
+        let mut sorted = bigrams.into_iter().collect::<Vec<_>>();
+        sorted.sort_by(|x, y| y.1.cmp(&x.1));
+
+        println!("Found {} bigrams.", sorted.len());
+        for (k, v) in sorted.iter().take(10) {
+            println!("{: <18} {}", format!("{}-{} :", k.s1, k.s2), v);
+        }
+    }
+
+    // This does work because 'bigrams', and therefore all references
+    // to words, are out of scope.
+    let inserted = words.insert("hello".to_string());
+    println!("hello in words?: {}", inserted);
+}
+
+fn main() {
+    // bigram_v1("data/pandp.txt");
+    bigram_with_extra_set("data/pandp.txt");
 }
